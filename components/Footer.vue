e3994bcb2d37923aa22c4bd37a29191f9aaa143b
fire8327 <fire83274@gmail.com>
2024-04-28 18:08:36 +0300

    upd footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,23 +9,23 @@
                 </div>
                 <div class="flex flex-col gap-8 max-xl:w-full lg:grow">
                     <div class="flex justify-center items-center gap-6">
-                        <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="skill-icons:github-dark"/></NuxtLink>
-                        <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="logos:telegram"/></NuxtLink>
-                        <NuxtLink class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="devicon:figma"/></NuxtLink>
+                        <NuxtLink to="https://github.com/m0useck" target="_blank" class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="skill-icons:github-dark"/></NuxtLink>
+                        <NuxtLink to="https://t.me/summerva" target="_blank" class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="logos:telegram"/></NuxtLink>
+                        <NuxtLink to="/" target="_blank" class="transition-all duration-300 hover:scale-125"><Icon class="text-3xl" name="devicon:figma"/></NuxtLink>
                     </div>
                     <div class="flex flex-col gap-4">
                         <div class="flex items-center justify-between xl:w-3/4 mx-auto gap-4 max-md:flex-col">
-                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">О нас</NuxtLink> 
+                            <NuxtLink to="/about" class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">О нас</NuxtLink> 
                             <span class="bg-white rounded-full w-1 h-1"></span>
-                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Проверка зрения</NuxtLink> 
+                            <NuxtLink to="/catalog" class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Каталог</NuxtLink> 
                             <span class="bg-white rounded-full w-1 h-1"></span>
-                            <NuxtLink class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Доставка</NuxtLink>
+                            <NuxtLink to="/delivery" class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">Доставка</NuxtLink>
                         </div>
                         <span class="bg-white rounded-full w-1 h-1 mx-auto md:hidden"></span>
                         <div class="flex items-center justify-between xl:w-3/4 mx-auto gap-4 max-md:flex-col">
                             <NuxtLink to="tel:8-999-888-77-66" class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">8-999-888-77-66</NuxtLink>
                             <span class="bg-white rounded-full w-1 h-1"></span>
-                            <NuxtLink to="mailto:123123123123@mail.ru" class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">123123123123@mail.ru</NuxtLink>
+                            <NuxtLink to="mailto:123123123123@mail.ru" class="flex flex-col w-fit after:w-0 after:h-px after:bg-white hover:after:w-full after:transition-all after:duration-300">example@mail.ru</NuxtLink>
                         </div>
                     </div>
                 </div>
