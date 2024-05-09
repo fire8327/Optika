5fefa98a7e750c8695d875b97308aee29ad8bff7
fire8327 <fire83274@gmail.com>
2024-05-09 05:13:48 +0300

    upd

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -6,7 +6,7 @@
             <span class="w-8 h-0.5 bg-black rounded-full"></span>
         </div>
         <FormKit @submit="addProduct" type="form" form-class="flex flex-col gap-6 full" :actions="false" messages-class="hidden">
-            <div class="flex flex-col gap-6 xl:gap-4 items-center w-full">
+            <div class="flex flex-col gap-6 items-center w-full">
                 <FormKit v-model="productForm.title" validation="required" outer-class="$remove:mb-4 w-full" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" name="Наименование" placeholder="Наименование товара" type="text"/>                    
                 <FormKit v-model="productForm.price" validation="required|number" outer-class="$remove:mb-4 w-full" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" name="Цена" placeholder="Цена" type="text"/>                    
                 <FormKit v-model="productForm.type" validation="required" outer-class="$remove:mb-4 w-full" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" name="Тип" placeholder="Тип товара" type="select" :options="['Готовые очки','Оправы','Линзы','Контактные линзы','Солнцезащитные очки']"/>                    
@@ -14,14 +14,22 @@
                 <FormKit v-model="productForm.brand" validation="required" outer-class="$remove:mb-4 w-full" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" name="Бренд" placeholder="Бренд" type="text"/>                    
                 <FormKit v-model="productForm.color" validation="required" outer-class="$remove:mb-4 w-full" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" name="Цвет" placeholder="Цвет товара" type="text"/>                    
                 <div class="flex flex-col gap-6 w-full">
-                    <div class="flex items-center max-lg:flex-col gap-2" v-for="characteristic in characteristics">
-                        <FormKit v-model="characteristic.title" validation="required" outer-class="$remove:mb-4 w-full lg:w-1/2" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" :name="`Наименование${characteristicCount}`" placeholder="Наименование характеристики" type="text"/>                    
-                        <FormKit v-model="characteristic.value" validation="required" outer-class="$remove:mb-4 w-full lg:w-1/2" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" :name="`Значение${characteristicCount}`" placeholder="Значение характеристики" type="text"/>                    
+                    <div class="flex flex-col gap-2 rounded-xl border border-[#3BBAC2] p-4" v-for="characteristic in characteristics">
+                        <button @click="deleteCharacteristic(characteristics.indexOf(characteristic))" type="button" class="px-4 py-2 rounded-xl text-white bg-[#3BBAC2] w-fit self-end">
+                            <Icon class="text-2xl" name="material-symbols:delete-forever"/>
+                        </button>
+                        <div class="flex items-center max-md:flex-col gap-2">
+                            <FormKit v-model="characteristic.title" validation="required" outer-class="$remove:mb-4 w-full md:w-1/2" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" :name="`Наименование${characteristicCount}`" placeholder="Наименование характеристики" type="text"/>                    
+                            <FormKit v-model="characteristic.value" validation="required" outer-class="$remove:mb-4 w-full md:w-1/2" inner-class="$remove:mb-1 $remove:max-w-md $remove:ring-1 $remove:ring-gray-400 w-full $remove:focus-within:ring-2" message-class="text-[#E71616]" input-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full" :name="`Значение${characteristicCount}`" placeholder="Значение характеристики" type="text"/>                    
+                        </div>
                     </div>
                 </div>
                 <button @click="addCharacteristic" type="button" class="px-4 py-2 rounded-xl text-white bg-[#3BBAC2] mx-auto">
                     <Icon class="text-2xl" name="material-symbols:exposure-plus-1"/>
                 </button>
+                <pre>
+                    {{ characteristics }}
+                </pre>
                 <FormKit @change="imageToBase" multiple="true" ref="inputImage" accept=".png,.jpg,.jpeg,.svg,.webp,.bmp" type="file" name="Фото" validation="required|min:3" messages-class="text-[#E9556D] font-Comfortaa text-base mt-2" outer-class="w-full" inner-class="px-4 py-2 border border-[#3BBAC2] rounded-xl focus:outline-none w-full bg-white"/>      
                 <FormKit type="submit" input-class="bg-[#3BBAC2] rounded-full font-semibold text-white text-center max-md:px-6 py-2 hover:opacity-80 transition-all duration-300 $remove:focus-visible:outline-blue-600 $remove:focus-visible:outline-offset-2 $remove:bg-blue-600 $remove:focus-visible:outline-2 $remove:inline-flex $remove:text-sm">Добавить</FormKit>
             </div>
@@ -203,13 +211,21 @@
             value: ""
         }
     ]) 
+
     const characteristicCount = ref(1)
-    const addCharacteristic = (slot) => {
+
+    const addCharacteristic = () => {
         characteristics.value.push({
             title: "",
             value: ""
         })
         characteristicCount.value++
+    }
+
+    const deleteCharacteristic = (id) => {
+        if(characteristics.value.length > 1) {
+            characteristics.value.splice(id, 1)
+        }
     }
 
 
